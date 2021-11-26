0abc6ed8f615aa28b2a6429c4a8392b34d499cc4
paukshto <dr.tvn@yandex.ru>
2021-11-26 04:20:57 +0300

    events added to stacking test

--- deploy/StackingTest.js
+++ deploy/StackingTest.js
@@ -1,3 +1,5 @@
+const tryVerify = require("./utils/tryVerify");
+
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
@@ -6,20 +8,32 @@ module.exports = async ({ getNamedAccounts, deployments }) => {
 
   const tokenArgs = [BigInt(10000000000000000000000 * 10 ** 18).toString()];
 
-  const token = await deploy("dummyPoolToken", {
+  await deploy("dummyPoolToken", {
     from: deployer,
     args: tokenArgs,
     log: true,
   });
 
-  const stacking = await deploy("PNLStacking", {
+  const token = await ethers.getContract("dummyPoolToken", deployer);
+
+  await deploy("PNLStacking", {
     from: deployer,
     args: [token.address],
     log: true,
   });
+  const stacking = await ethers.getContract("PNLStacking", deployer);
 
   await token.transfer(stacking.address, BigInt(100000000 * 10 ** 18));
-  await token.transfer(testerAcc, BigInt(5000 * 10 ** 18));
+  await token.transfer(testerAcc, BigInt(50000 * 10 ** 18));
+  await token.transfer(deployer, BigInt(50000 * 10 ** 18));
+  await token.approve(stacking.address, BigInt(10000000000 * 10 ** 18));
+  await stacking.stack(BigInt(1000 * 10 ** 18), 0);
+  await stacking.stack(BigInt(1000 * 10 ** 18), 1);
+  await stacking.stack(BigInt(1000 * 10 ** 18), 2);
+  await stacking.stack(BigInt(1000 * 10 ** 18), 3);
+  await stacking.stack(BigInt(1000 * 10 ** 18), 0);
+  await tryVerify(stacking.address, [token.address]);
+  await tryVerify(token.address, tokenArgs);
 
   console.log(`Stacking address: ${stacking.address}`);
   console.log(`Token address: ${token.address}`);
